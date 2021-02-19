ed173e753ac831200ce7ff0ec79d941c29b52594
Remzi <remzitdf@abv.bg>
2021-02-19 10:04:15 +0200

    Add files via upload

---
+++ ChangeList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split();
+                string firstWordFromCommand = tokens[0];
+                if (firstWordFromCommand == "Delete")
+                {
+                    int element = int.Parse(tokens[1]);
+                    numbers.RemoveAll(x=> x==element);
+                }
+                else if (firstWordFromCommand == "Insert")
+                {
+                    int element = int.Parse(tokens[1]);
+                    int position = int.Parse(tokens[2]);
+                    numbers.Insert(position,element);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
---
+++ HouseParty/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int commands = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+
+            for (int i = 0; i < commands; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                if (tokens.Length==3)
+                {
+                    if (guests.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(name);
+                    }
+                }
+                else
+                {
+                    bool removed = guests.Remove(name);
+
+                    if (!removed)
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("\n",guests));
+        }
+    }
+}
